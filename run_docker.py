16b98f89902d86293317d9e10af9910f0767db1b
verena <9377970+vpchung@users.noreply.github.com>
2022-07-19 15:51:00 -0700

    add runtime for gpu usage

--- run_docker.py
+++ run_docker.py
@@ -127,7 +127,8 @@ def main(syn, args):
                                               name=container_name,
                                               network_disabled=True,
                                               mem_limit='6g',
-                                              stderr=True)
+                                              stderr=True,
+                                              runtime="nvidia")
         except docker.errors.APIError as err:
             container = None
             remove_docker_container(container_name)
